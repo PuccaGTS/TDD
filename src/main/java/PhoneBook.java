ea27d8e8d223e8d1be7337116ee7029eec5bb61f
Vladislav Pavlenko <pavlenkoovd@gmail.com>
2022-12-16 23:24:31 +0900

    add implemented

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,8 +1,11 @@
-public class PhoneBook {
+import java.util.HashMap;
+import java.util.Map;
 
+public class PhoneBook {
+    private Map<String,String> contacts = new HashMap<>();
 
     public int add(String name, String number){
-
-        return 0;
+        contacts.put(name, number);
+        return contacts.size();
     }
 }
